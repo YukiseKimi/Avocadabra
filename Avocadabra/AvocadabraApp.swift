dba9361fe7949f9fec04320330af49d79cf9f123
YukiseKimi <mannychau@gmail.com>
2021-10-13 12:01:42 +0100

    Initial Commit

---
+++ Avocadabra/AvocadabraApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadabraApp.swift
+//  Avocadabra
+//
+//  Created by Manny Chau on 2021-10-13.
+//
+
+import SwiftUI
+
+@main
+struct AvocadabraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
