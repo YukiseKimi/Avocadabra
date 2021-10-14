f62b00426c827b2b8104fa79511b5e0bfeb7b412
YukiseKimi <mannychau@gmail.com>
2021-10-14 14:10:27 +0100

    Header View

--- Avocadabra/View/AppView.swift
+++ Avocadabra/View/AppView.swift
@@ -40,5 +40,6 @@ struct AppView: View {
 struct AppView_Previews: PreviewProvider {
     static var previews: some View {
         AppView()
+        
     }
 }
--- Avocadabra/View/ContentView.swift
+++ Avocadabra/View/ContentView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Avocado Recipes")
-            .padding()
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                // MARK: -header
+                
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack(alignment: .top, spacing: 0) {
+                        HeaderView()
+                    }
+                }
+                
+                
+                // MARK: -footer
+                
+                VStack(alignment: .center, spacing: 20) {
+                    Text("All about Avocados")
+                        .font(.system(.title, design: .serif))
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("ColorGreenAdaptive"))
+                        .padding(18)
+                    Text("Everything you wanted to know about avocados but too afraid to ask.")
+                        .font(.system(.body, design: .serif))
+                        .multilineTextAlignment(.center)
+                        .foregroundColor(Color.gray)
+                }
+                .frame(maxWidth: 640)
+                .padding()
+                .padding(.bottom, 85)
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+        .padding(0)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+        
     }
 }
