156472173ab31f9f9d8c8003d5e6f4f2b8548db1
YukiseKimi <mannychau@gmail.com>
2021-10-16 14:22:52 +0100

    Adding HeaderData to HeaderView

---
+++ Avocadabra/Model/HeaderModel.swift
@@ -0,0 +1,18 @@
+//
+//  HeaderModel.swift
+//  Avocadabra
+//
+//  Created by Manny Chau on 2021-10-16.
+//
+
+import SwiftUI
+
+// MARK: - HEADER MODEL
+
+struct Header: Identifiable {
+    var id = UUID()
+    var image: String
+    var headline: String
+    var subheadline: String
+    
+}
--- Avocadabra/View/HeaderView.swift
+++ Avocadabra/View/HeaderView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct HeaderView: View {
     // MARK: - Properties
     
+    var header: Header
+    
     @State private var showHeadline: Bool = false
     
     var slideInAnimation: Animation {
@@ -21,7 +23,7 @@ struct HeaderView: View {
     
     var body: some View {
         ZStack {
-            Image("avocado-slice-1")
+            Image(header.image)
                 .resizable()
                 .aspectRatio(contentMode: .fill)
             HStack(alignment: .top, spacing: 0) {
@@ -30,13 +32,13 @@ struct HeaderView: View {
                     .frame(width: 4)
                            
                 VStack (alignment: .leading, spacing: 6) {
-                    Text("Avocado")
+                    Text(header.headline)
                         .font(.system(.title, design: .serif))
                         .fontWeight(.bold)
                         .foregroundColor(Color.white)
                         .shadow(radius: 3)
                         
-                    Text("Avocados are a powerhouse meal ingredient at any meal for anyone.")
+                    Text(header.subheadline)
                         .font(.footnote)
                         .lineLimit(2)
                         .multilineTextAlignment(.leading)
@@ -61,7 +63,7 @@ struct HeaderView: View {
 
 struct HeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        HeaderView()
+        HeaderView(header: headersData[1])
             .previewLayout(.sizeThatFits)
 //            .environment(\.colorScheme, .dark)
     }
